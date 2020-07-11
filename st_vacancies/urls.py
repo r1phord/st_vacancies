44868d2285756c32efdafc97f2e0b7b9de3116e5
r1phord <vlad.gorenkov.1996@mail.ru>
2020-07-12 01:01:15 +0200

    add project main files

---
+++ st_vacancies/urls.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth.views import LogoutView
+from django.urls import path, include
+
+from vacancies.views import MainView, CompanyView, custom_handler500, custom_handler404, MyLoginView, RegisterView, \
+    ResumeView
+
+handler404 = custom_handler404
+handler500 = custom_handler500
+
+urlpatterns = [
+    path('', MainView.as_view(), name='main'),
+    path('admin/', admin.site.urls),
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('companies/<int:company_id>/', CompanyView.as_view(), name='company'),
+    path('myresume', ResumeView.as_view(), name='myresume'),
+    path('vacancies/', include('vacancies.urls')),
+    path('mycompany/', include('mycompany.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
